3328b9339809d8fd7711421743c017252cae18c0
Aswin G <aswing2003@gmail.com>
2026-06-02 13:32:57 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+def main():
+    
+    students=[] # to collect and save student details
+    status = True
+    while status:
+        print('========Menu========')
+        print('1. Add Student')
+        print('2. Add scholarship student')
+        print('3. Display all students')
+        print('4. Exit')
+        user_input = input('Enter your choice: ')
+        if user_input == '1':
+            #methods to add students
+            pass
+        elif user_input == "2":
+            #methods to add scholarship students 
+            pass
+        elif user_input == "3":
+            #method to display all stidents
+            print("yo bro")
+        
+        elif user_input == "4":
+            status = False
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+        
+        
+main()
+        
+        
+        
+        
+        
+        
+        
+            #initialize python project
+    #if _(fucntion) def main() -> None
+       # main()
